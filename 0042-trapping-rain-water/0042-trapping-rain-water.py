9f7583721366881c71af4bcac82baa70871cd58a
ayushdub3y <142671260+ayushdub3y@users.noreply.github.com>
2026-08-18 21:11:31 +0530

    Time: 5 ms (79.18%), Space: 21 MB (84.34%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,33 +1,19 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        #the first part is to write two helper functions to get ourselves the left max and right max for each index.
-        #the second part is to find us for every ith index the height = min(lm[i], rm[i]) - h[i]and keep adding it to the  res
-
-        res = 0
+        #the two pointer apporach describes maintaining two pointers from left to right where the local maximum or current maximum on both sides will allow us to help us calculate the amount of water
+        #we will increment the left pointer if height at left is smaller than right and vice versa.
         n = len(height)
+        left, right = 0, n-1
+        left_max, right_max = height[0], height[n-1]
+        res= 0
 
-        def leftMax(height):
-            maximum = [0] * len(height)
-            running_max = height[0]
-            maximum[0] = running_max
-            for i in range(1, len(height)):
-                running_max = max(running_max, height[i])
-                maximum[i] = running_max
-            return maximum
-        
-        def rightMax(height):
-            maximum = [0] * len(height)
-            running_max = height[n-1]
-            maximum[n-1] = running_max
-
-            for i in range(len(height)-2, -1, -1):
-                running_max = max(running_max, height[i])
-                maximum[i] = running_max
-            return maximum
-        left_max= leftMax(height)
-        right_max =rightMax(height)
-
-        for i in range(len(height)-1):
-            heights = min(left_max[i], right_max[i]) - height[i]
-            res+=heights
-        return res   
+        while left < right:
+            if left_max < right_max:
+                left+=1
+                left_max = max(left_max, height[left])
+                res += left_max - height[left]
+            else:
+                right -=1
+                right_max = max(right_max, height[right])
+                res += right_max - height[right]
+        return res
