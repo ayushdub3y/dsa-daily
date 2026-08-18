e3ee7e085d9c2ab2ffc1d52d9e496d29e8c77b21
ayushdub3y <142671260+ayushdub3y@users.noreply.github.com>
2026-08-18 13:03:22 +0530

    Time: 0 ms (100%), Space: 19.4 MB (22.66%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,16 +1,15 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        low, mid = 0,0
-        n = len(nums) - 1
-        high = n
+        hashmap = {0:0, 1:0, 2:0}
 
-        while mid <= high:
-            if nums[mid] < 1:
-                nums[low], nums[mid] = nums[mid], nums[low]
-                low +=1
-                mid+=1
-            elif nums[mid] == 1:
-                mid+=1
-            else: #nums mid == 2
-                nums[high], nums[mid] = nums[mid], nums[high]
-                high-=1
+        for num in nums:
+            hashmap[num] +=1
+        
+        nums.clear()
+
+        for i in range(hashmap[0]):
+            nums.append(0)
+        for i in range(hashmap[1]):
+            nums.append(1)
+        for i in range(hashmap[2]):
+            nums.append(2)
