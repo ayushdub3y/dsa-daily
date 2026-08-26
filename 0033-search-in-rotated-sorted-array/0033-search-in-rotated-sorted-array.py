9a762161688110ba51b85e28e73c1053d8a16523
ayushdub3y <142671260+ayushdub3y@users.noreply.github.com>
2026-08-26 21:17:12 +0530

    Time: 0 ms (100%), Space: 19.2 MB (96.23%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -4,27 +4,19 @@ class Solution:
         right = len(nums) - 1
 
         while left <= right:
-            mid = (left + right) // 2
+            mid = (left+right) // 2
 
-            if nums[mid] == target:
+            if target == nums[mid]:
                 return mid
 
-            # Left half is sorted
-            if nums[left] <= nums[mid]:
-
-                # Target lies inside left sorted half
-                if nums[left] <= target < nums[mid]:
+            if nums[left] <= nums[mid]: #left side of array is sorted
+                if nums[left] <= target < nums[mid]: #target belongs in between left and mid
                     right = mid - 1
                 else:
                     left = mid + 1
-
-            # Right half is sorted
             else:
-
-                # Target lies inside right sorted half
                 if nums[mid] < target <= nums[right]:
-                    left = mid + 1
-                else:
-                    right = mid - 1
-
+                    left = mid+1
+                else: 
+                    right = mid -1
         return -1
